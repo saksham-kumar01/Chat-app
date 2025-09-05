7fe2db3454a574b1863484c2d4f781bb55294777
Samrajput1 <samrajput7835@gmail.com>
2025-09-05 20:54:53 +0530

    feat: Revamp Chat component with enhanced layout and styling
    - Updated Chat.css to improve the layout with borders, flex properties, and background colors.
    - Enhanced Chat.jsx to include user information, icons, and input functionality for a more interactive chat experience.
    - Removed unnecessary border from List.css for a cleaner design.

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -4,7 +4,36 @@ import './Chat.css'
 const Chat = () => {
   return (
     <div className='chat'>
-      chat
+      <div className="top">
+        <div className="user">
+          <img src="./avatar.png" alt="" />
+          <div className="texts">
+            <span>SAM</span>
+            <p>let's go</p>
+          </div>
+        </div>
+        <div className="icons">
+          <img src="./phone.png" alt="" />
+          <img src="./video.png" alt="" />
+          <img src="./info.png" alt="" />
+        </div>
+      </div>
+      <div className="centre"></div>
+      <div className="bottom">
+        <div className="emogi">
+            <img src="./emoji.png" alt="" />
+        </div>
+        <div className="attach" id="icons">
+        <img src="./attach.png" alt="" />
+        </div>
+        <div className="input">
+             <input type="text" placeholder='Type a Message...' />
+             <button className="send">Send</button>
+        </div>
+        <div className='mic' id="icons">
+          <img src="./mic.png" alt="" />
+        </div>
+      </div>
     </div>
   )
 }
